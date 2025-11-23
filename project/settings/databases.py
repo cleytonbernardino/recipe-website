7ab51f196927ae02bd31a66bc485e741f6fafe1f
cleytonbernardino <100251591+cleytonbernardino@users.noreply.github.com>
2025-11-23 20:54:35 -0300

    Feat: MySQL support

--- project/settings/databases.py
+++ project/settings/databases.py
@@ -1,8 +1,16 @@
 from .environment import BASE_DIR
 
 DATABASES = {
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.sqlite3',
+    #     'NAME': BASE_DIR / 'db.sqlite3',
+    # }
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'RecipeBook',  
+        'USER': 'root',   
+        'PASSWORD': '2270',
+        'HOST': 'localhost',
+        'PORT': '3306', 
     }
 }
